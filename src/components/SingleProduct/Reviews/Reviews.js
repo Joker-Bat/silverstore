35d6aa0ae7e37e14521c3e93276ea15c6710fd5e
Joker-Bat <shanmugam091098@gmail.com>
2021-05-17 17:05:14 +0530

    Added star hover style and textarea counter in Reviews

--- src/components/SingleProduct/Reviews/Reviews.js
+++ src/components/SingleProduct/Reviews/Reviews.js
@@ -7,18 +7,87 @@ import classes from "./Reviews.module.scss";
 import SimpleButton from "../../UI/SimpleButton/SimpleButton";
 import Backdrop from "../../UI/Backdrop/Backdrop";
 
-const Reviews = ({ ratings }) => {
-  const [addReview, setAddReview] = useState(false);
+// HelperFunction
+const getTitleByRating = (value) => {
+  switch (+value) {
+    case 1:
+      return "i hate it";
+    case 2:
+      return "average one";
+    case 3:
+      return "not too bad";
+    case 4:
+      return "i like it";
+    case 5:
+      return "just awesome";
+    default:
+      return "your review";
+  }
+};
 
+const maxReviewLength = 150;
+
+const Reviews = ({ ratings }) => {
+  // Open or close Add Review Container
+  const [addReview, setAddReview] = useState(false);
+  // Value handler for review textarea and maximum text value
+  const [reviewText, setReviewText] = useState("");
+  const [reviewLength, setReviewLength] = useState(maxReviewLength);
+
+  // Stars clicked or hover state
+  const [currentStar, setCurrentStar] = useState(0);
+  const [hoverStar, setHoverStar] = useState(undefined);
+  const [fiveStar, setFiveStar] = useState(false);
+  // ReviewTitle base on star
+  const [reviewTitle, setReviewTitle] = useState("your review");
+  // Get an duplicate array to render 5 stars
   const starsCount = Array(5).fill(0);
 
+  // Open or close Add Review Container
   const openReviewContainer = () => {
     setAddReview(true);
   };
-
   const closeReviewContainer = (e) => {
     e.preventDefault();
     setAddReview(false);
+  };
+  // Value handler for review textarea
+  const handleReviewText = (e) => {
+    const currentValue = e.target.value;
+    setReviewText(currentValue);
+    setReviewLength((prev) => {
+      if (currentValue.length <= maxReviewLength) {
+        return maxReviewLength - currentValue.length;
+      } else {
+        return prev;
+      }
+    });
+  };
+
+  // Stars hover handler (mouseOver and mouseOut)
+  const starMouseOverHandler = (value) => {
+    setHoverStar(value);
+    // Give Review Title based on Rating
+    const currentTitle = getTitleByRating(value);
+    setReviewTitle(currentTitle);
+  };
+  const starMouseLeaveHandler = () => {
+    setHoverStar(undefined);
+    // Remove review title if no star selected
+    if (currentStar) {
+      const currentTitle = getTitleByRating(currentStar);
+      setReviewTitle(currentTitle);
+    } else {
+      setReviewTitle("your review");
+    }
+  };
+  const starClickHandler = (value) => {
+    setCurrentStar(value);
+    if (+value === 5) {
+      setFiveStar(true);
+    } else {
+      setFiveStar(false);
+    }
   };
 
   return (
@@ -69,20 +138,39 @@ const Reviews = ({ ratings }) => {
         >
           <div className={classes.StarContainer}>
             {starsCount.map((_, index) => {
+              const starClass = [
+                classes.Star,
+                (currentStar || hoverStar) >= index + 1 && classes.Active,
+                fiveStar && classes.FiveStar,
+              ];
               return (
-                <span key={`ReviewStars${index}`} className={classes.Star}>
+                <span
+                  key={`ReviewStars${index}`}
+                  className={starClass.join(" ")}
+                  onMouseOver={() => starMouseOverHandler(index + 1)}
+                  onMouseLeave={() => starMouseLeaveHandler()}
+                  onClick={() => starClickHandler(index + 1)}
+                >
                   <i className="fas fa-star"></i>
                 </span>
               );
             })}
           </div>
-          <h1 className={classes.ReviewTitle}>I like it</h1>
+          <h1 className={classes.ReviewTitle}>{reviewTitle}</h1>
           <form className={classes.FormContainer}>
-            <textarea
-              cols="30"
-              rows="6"
-              placeholder="leave your review..."
-            ></textarea>
+            <div className={classes.ReviewInputContainer}>
+              <textarea
+                cols="30"
+                rows="6"
+                placeholder="leave your review..."
+                maxLength={maxReviewLength}
+                value={reviewText}
+                onChange={handleReviewText}
+              ></textarea>
+              <div className={classes.ReviewLength}>
+                <p>{reviewLength}</p>
+              </div>
+            </div>
             <SimpleButton
               name="post"
               capitalize
