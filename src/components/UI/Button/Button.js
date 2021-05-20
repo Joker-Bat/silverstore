2f11cc43da6ac6a0a484783fbe561395fe51fa5f
Joker-Bat <shanmugam091098@gmail.com>
2021-05-20 11:42:31 +0530

    Button Size changed

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -6,12 +6,13 @@ import classes from "./Button.module.scss";
 // React Router
 import { Link } from "react-router-dom";
 
-const Button = ({ small, large, uppercase, name, route, clicked }) => {
+const Button = ({ small, large, uppercase, name, route, clicked, capitalize }) => {
   const buttonClass = [classes.Button];
 
   small && buttonClass.push(classes.Small);
   large && buttonClass.push(classes.Large);
   uppercase && buttonClass.push(classes.Uppercase);
+  capitalize && buttonClass.push(classes.Capitalize);
 
   return (
     <Link to={route} className={buttonClass.join(" ")} onClick={clicked}>
