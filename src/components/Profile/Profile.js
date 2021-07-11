a20eb32727fdfeec4f4faf36a63972ba40fcc067
Joker-Bat <shanmugam091098@gmail.com>
2021-07-11 09:35:22 +0530

    Profile UI designed

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
+// Styles
+import classes from "./Profile.module.scss";
 // Axios
 import axios from "axios";
 // Redux toolkit
 import { useDispatch } from "react-redux";
 import { removeToken } from "../../store/auth/authSlice";
+// Temp
+import image from "../../images/emoji/eyeClick.webp";
+// Components
+import SimpleButton from "../UI/SimpleButton/SimpleButton";
+import Title from "../UI/Title/Title";
 
 const Profile = () => {
   const dispatch = useDispatch();
@@ -24,11 +31,75 @@ const Profile = () => {
   };
 
   return (
-    <div>
-      <h1>Profile Page</h1>
-      <h2>{user.name} Welcome to your profile</h2>
+    <div className={classes.Profile}>
+      <Title name="profile" />
+      <div className={classes.ProfileContainer}>
+        <div className={classes.UpdateProfileContainer}>
+          <div className={classes.ProfilePictureContainer}>
+            <img src={image} alt="emoji" />
+          </div>
+          <form>
+            <input disabled type="file" id="profilePic" accept="image/*" />
+            <label htmlFor="profilePic">
+              Select new picture hehr hehrhehe
+              <span
+                style={{ width: "40%" }}
+                className={classes.Progress}
+              ></span>
+            </label>
+            <SimpleButton name="upload" small capitalize />
+          </form>
+        </div>
+        <div className={classes.UpdateDetailContainer}>
+          <div className={classes.UpdateUserContainer}>
+            <h2>update profile</h2>
+            <form>
+              <input type="text" value="username" />
+              <input type="email" value="test@test.com" />
+              <SimpleButton name="update" small capitalize />
+            </form>
+          </div>
+          <div className={classes.UpdatePasswordContainer}>
+            <h2>update password</h2>
+            <form>
+              <div className={classes.InputGroup}>
+                <label htmlFor="oldPassword">old password</label>
+                <input type="password" value="password" id="oldPassword" />
+              </div>
+              <div className={classes.InputGroup}>
+                <label htmlFor="newPassword">new password</label>
+                <input type="password" value="password" id="newPassword" />
+              </div>
+              <div className={classes.InputGroup}>
+                <label htmlFor="newPasswordConfirm">new password confirm</label>
+                <input
+                  type="password"
+                  value="password"
+                  id="newPasswordConfirm"
+                />
+              </div>
+              <div className={classes.ShowPassword}>
+                <input
+                  type="checkbox"
+                  name="showPassword"
+                  id="showPassword"
+                  className={classes.Checkbox}
+                />
 
-      <button onClick={handleLogout}>Logout</button>
+                <label htmlFor="showPassword" className={classes.Label}>
+                  <span className={classes.Checkmark}></span>
+                  Show password
+                </label>
+              </div>
+              <SimpleButton name="update password" small capitalize />
+            </form>
+          </div>
+          <SimpleButton name="logout" small capitalize />
+        </div>
+      </div>
+      {/* <h2>{user.name} Welcome to your profile</h2>
+
+      <button onClick={handleLogout}>Logout</button> */}
     </div>
   );
 };
