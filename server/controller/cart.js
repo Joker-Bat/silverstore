4b93b478cab238de219f4d4431b6d4700486c6c5
Joker-Bat <shanmugam091098@gmail.com>
2021-07-11 20:51:55 +0530

    Added getting all carts

--- server/controller/cart.js
+++ server/controller/cart.js
@@ -1,6 +1,47 @@
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const Cart = require("../models/cart");
+const User = require("../models/user");
+
 exports.getWelcome = (req, res, next) => {
   res.status(200).json({
     status: "success",
     message: "Connected",
   });
 };
+
+exports.addCart = catchAsync(async (req, res, next) => {
+  const { id } = req.user;
+  const { productId, count } = req.body;
+
+  const user = await User.findById(id);
+  if (!user) return next(new AppError("No user with that id", 400));
+
+  const cart = await Cart.findOne({ user: id });
+  if (!cart) {
+    await Cart.create({ user: id, $push: { products: { productId, count } } });
+  } else {
+    await Cart.findOneAndUpdate(
+      { user: id },
+      { $push: { products: { productId, count } } }
+    );
+  }
+
+  res.status(200).json({
+    status: "success",
+    message: "Cart Saved",
+  });
+});
+
+exports.getAllCarts = catchAsync(async (req, res, next) => {
+  const { id } = req.user;
+
+  const carts = await Cart.find({ user: id });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      carts,
+    },
+  });
+});
--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
-const { getWelcome } = require("../controller/cart");
+const { getWelcome, addCart, getAllCarts } = require("../controller/cart");
+const { protect } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.get("/", getWelcome);
+
+router.post("/add", protect, addCart);
+router.get("/getall", protect, getAllCarts);
 
 module.exports = router;
