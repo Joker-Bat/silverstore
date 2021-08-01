f16dba9372b0f612147bcc05e99bc75f30b3cbbc
Joker-Bat <shanmugam091098@gmail.com>
2021-08-01 17:09:40 +0530

    Changed some config for deployment

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ app.use('/api/v1/reviews', reviewRouter);
 
 // Serve my frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../build')));
+  app.use(express.static(path.resolve(__dirname, '../build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
   });
