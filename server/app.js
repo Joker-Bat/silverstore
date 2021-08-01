2f0e064d0a2f6254712f8fa6ba457410d34543b3
Joker-Bat <shanmugam091098@gmail.com>
2021-08-01 19:05:36 +0530

    Small change in file path

--- server/app.js
+++ server/app.js
@@ -39,9 +39,9 @@ app.use('/api/v1/reviews', reviewRouter);
 
 // Serve my frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join('../build')));
+  app.use(express.static(path.join(__dirname, '../build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join('../build/index.html'));
+    res.sendFile(path.join(__dirname, '../build/', 'index.html'));
   });
 }
 
